2b43f4e1f40a7b7a8b1dbd3ee50422545b62c0db
Shahzeb khan <shahzeb.khan@sap.com>
2019-07-02 15:11:45 +0200

    Update karma-ci.conf.js

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
       "RemoteChrome": {
         base: "WebDriver",
         config: {
-          hostname: "localhost",
+          hostname: "cloudl000024.wdf.sap.corp",
           port: 4444
         },
         browserName: "chrome",
