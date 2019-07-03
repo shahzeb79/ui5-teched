b4251ea7e8a48b0256dccc399d46733fda165a9c
Shahzeb khan <shahzeb.khan@sap.com>
2019-07-03 15:01:39 +0200

    Update karma-ci.conf.js

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -11,6 +11,7 @@ module.exports = function (config) {
     customLaunchers: {
       "RemoteChrome": {
         base: "WebDriver",
+        flags: ['--screenshot'],
         config: {
           hostname: "selenium",
           port: 4444
